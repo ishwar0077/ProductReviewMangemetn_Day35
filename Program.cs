154c40c2c0b8fd0e75ad710bf840088b42f2bba1
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-11-18 21:17:34 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductReview_Day35
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Mangagement Program");
+        }
+    
+    }
+
+}
